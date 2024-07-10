c29773ae3c10c12f53144dd760a5139ba1b4af06
Muhammed Irfaan <muhammedirfaan1@gmail.com>
2024-07-10 09:16:42 +0200

    Create cell_mo.py
    Tried my hand at this. I know theres a lot mistakes but it helped me get a better understanding

---
+++ cell_mo.py
@@ -0,0 +1,20 @@
+class Cell:
+  
+    def __init__(self, x_coord: str, y_coord: str):
+        self.x_coord = x_coord
+        self.y_coord = f"SOME RANDOM TEXT: {y_coord}"
+
+    def set_value (self, value, x_coord, y_coord)
+      self.value(x_coord,y_coord) = value
+      return value
+
+    def get_value (self):
+      return self.value
+
+    def coord_value(self, x_coord, y_coord):
+      coord_value=self.(x_coord,y_coord).value
+      return coord_value
+
+      
+        
+        
