4cbc4bb245b595be99bdcea5e6ac89a4b7db1c42
explorer-timtam <timothy@explore.ai>
2024-07-10 10:47:47 +0200

    CG Chat 20240710

--- cell_mo.py
+++ cell_mo.py
@@ -1,20 +1,20 @@
 class Cell:
   
     def __init__(self, x_coord: str, y_coord: str):
+        """Give a description of what a contructor is, as well as how you are using it here."""
         self.x_coord = x_coord
-        self.y_coord = f"SOME RANDOM TEXT: {y_coord}"
+        self.y_coord = y_coord
+        # Randomly assign self.value to X, O or .
+        self.value = "."
 
-    def set_value (self, value, x_coord, y_coord)
-      self.value(x_coord,y_coord) = value
-      return value
+    def set_value (self, value):
+      self.value = value
+      return "Value set correctly"
 
     def get_value (self):
       return self.value
-
-    def coord_value(self, x_coord, y_coord):
-      coord_value=self.(x_coord,y_coord).value
-      return coord_value
-
+    
+    # Write a validater to check that the value is correct (X or O or .)
       
         
         
--- grid.py
+++ grid.py
@@ -1,21 +1,40 @@
-from cell import Cell
+# 3x3 = 9 cells
+# Every cell has a X and Y coord. 
 
-cell_1 = Cell(x_coord="1", y_coord="3")
-cell_2 = Cell(x_coord="3", y_coord="2")
+# game_board = [[1][2][3]
+#               [4][5][6]
+#               [7][8][9]]
+
+from cell_mo import Cell
+
+c1 = Cell(x_coord=1, y_coord=1)
+c2 = Cell(x_coord=2, y_coord=1)
+
+grid = []
+
+# grid = [
+#     [Cell(1,1), Cell(1, 2), Cell(1, 3)],
+#     [Cell(2,1), Cell(2, 2), Cell(2, 3)],
+#     [Cell(3,1), Cell(3, 2), Cell(3, 3)],
+# ]
+
+row = 3
+col = 3
 
 
-cell_1.WhereAmI()
-
-cell_2.WhereAmI()
-
-
-print(cell_1)
+for r in range(row):
+    for c in range(col):
+            print(f"{r}:{c}")
+            grid.append(Cell(r, c))
 
 
-def distance(c1: Cell, c2: Cell):
-    print("HERE")
+# Next Step:
+# - Document your class with all its functions.
+# - Create a 100x100 game grid that is:
+    # An array of arrays.
+        # (See line 15)
+        # To Access a cell at 1:2: grid[1][2]
+# - Create a for loop that outputs a structured game board
+# - Write that output to a text file.
 
-
-
-
-distance(cell_1, cell_2)
+print("x_coord\t\ty_coord")
